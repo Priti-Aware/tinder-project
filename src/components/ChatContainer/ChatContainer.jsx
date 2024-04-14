e77714d163c6e1fc69ef0cec8665e2629498851e
Priti <pritiaware123@gmail.com>
2024-04-14 12:16:59 +0530

    Dashboard UI ✅

---
+++ src/components/ChatContainer/ChatContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ChatContainer.css'
+const ChatContainer = () => {
+
+  return (
+    <div className='chat-container'>
+      Chat Container
+    </div>
+  );
+}
+
+export default ChatContainer;
